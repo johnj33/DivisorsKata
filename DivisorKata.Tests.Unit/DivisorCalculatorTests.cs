7962758624e8040645b8ba361cdbb75d4311e0dd
PriyankaH22 <priyanka.hosur@oneadvanced.com>
2016-11-22 14:39:39 +0530

    first cycle

--- DivisorKata.Tests.Unit/DivisorCalculatorTests.cs
+++ DivisorKata.Tests.Unit/DivisorCalculatorTests.cs
@@ -14,7 +14,8 @@ namespace DivisorKata.Tests.Unit
             _divisorCalculator = new DivisorCalculator();
         }
 
-        [TestCase(8, new[] {1,2,4})]
+        [TestCase(1, new[] {1})]
+        [TestCase(2, new[] {1,2})]
         public void WhenCalculateIsCalledThenTheExpectedResponseIsReturned(int inputNumber, int[] expectedResponse)
         {
             var response = _divisorCalculator.Calculate(inputNumber);
--- DivisorsKata/DivisorCalculator.cs
+++ DivisorsKata/DivisorCalculator.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 
 namespace DivisorsKata
@@ -8,17 +7,7 @@ namespace DivisorsKata
     {
         public int[] Calculate(int inputNumber)
         {
-            List<int> divisor = new List<int>();
-  
-            for (int x = 1; x <= inputNumber; x++)
-            {
-                if (inputNumber%x == 0)
-                {
-                    divisor.Add(inputNumber);
-                }
-              
-            }
-            return divisor.ToArray();
+            return new[] { 1 };
         }
     }
 }
