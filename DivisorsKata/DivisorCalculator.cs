32e8fadce284a5cccccdbba774f5243ba6370c29
PriyankaH22 <priyanka.hosur@oneadvanced.com>
2016-11-22 18:26:37 +0530

    my changes

--- DivisorsKata/DivisorCalculator.cs
+++ DivisorsKata/DivisorCalculator.cs
@@ -4,19 +4,20 @@
     {
         public int[] Calculate(int inputNumber)
         {
-           
+
             switch (inputNumber)
             {
                 case 1:
-                    return new[] {1};
+                    return new[] { 1 };
                 case 2:
-                    return new[] {1, 2};
+                    return new[] { 1, 2 };
                 case 3:
-                    return new[] {1, 3};
+                    return new[] { 1, 3 };
             }
             return null;
         }
-       
     }
+
+    
 }
 
